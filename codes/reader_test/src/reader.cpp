aef4638f33416691d509dd82e1c79b69207720cd
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-08-11 17:13:14 +0200

    multi-threading test with openmp and c++ threads

--- codes/reader_test/src/reader.cpp
+++ codes/reader_test/src/reader.cpp
@@ -20,28 +20,83 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-// #include "kadath_adapted_polar.hpp"
-// #include "kadath_adapted.hpp"
 #include "Configurator/config_bco.hpp"
-// #include "bco_utilities.hpp"
-// #include "EOS/EOS.hh"
-// #include "coord_fields.hpp"
-// #include "mpi.h"
 #include "Solvers/reader.hpp"
 #include "kadath_adapted.hpp"
 #include "kadath_adapted_bh.hpp"
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
+#include<thread>
 
 using namespace Kadath;
 using namespace Kadath::FUKA_Config;
+  
+  using config_t = kadath_config_boost<BCO_BH_INFO>;
+  using reader_t = Kadath::FUKA_Solvers::CFMS_BH_Reader<config_t, Space_adapted_bh>;
+  using ary_t = std::array<double, reader_t::OUTPUT_VARS::NUM_OUTPUT_VARS>;
+
+constexpr unsigned int Npts = 128;  
+constexpr double range = 3;
+constexpr double dx = range / Npts;
+
+void fill_coords(std::vector<double>& xx, std::vector<double>& yy, std::vector<double>& zz) {
+  unsigned int nthreads = std::thread::hardware_concurrency();
+  unsigned int chunksize = Npts / nthreads;
+
+  auto fill_points = [&](auto start, auto stop) {
+    for(auto i = start; i < stop; ++i) {
+      xx[i]+= i * dx;
+      yy[i]+= i * dx;
+      zz[i]+= i * dx;
+    }
+  };
+
+  std::vector<std::thread> threads;
+  for(auto j = 0; j < nthreads; ++j) {
+    auto start = chunksize * j;
+    auto stop = (chunksize * (j+1) > Npts) ? Npts : chunksize * (j+1);
+    threads.push_back(std::thread(fill_points, start, stop));
+  }
+  for(auto j = 0; j < nthreads; ++j) {
+    threads[j].join();
+  }
+}
+
+ary_t interpolate(std::string fn, std::vector<double>& xx, std::vector<double>& yy, std::vector<double>& zz) {
+
+  
+  config_t bconfig(fn);  
+  reader_t input_reader(fn);
+
+  unsigned int nthreads = std::thread::hardware_concurrency();
+  unsigned int chunksize = Npts / nthreads;
+
+  std::vector<ary_t> all;
+
+  auto interp_points = [&](auto start, auto stop) {
+    for(auto i = start; i < stop; ++i) {
+      reader_t reader(input_reader);
+      auto vals = reader.export_pointwise(xx[i], yy[i], zz[i]);
+      all.push_back(vals);
+      // xx[i]+= i * dx;
+      // yy[i]+= i * dx;
+      // zz[i]+= i * dx;
+    }
+  };
+
+  std::vector<std::thread> threads;
+  for(auto j = 0; j < nthreads; ++j) {
+    auto start = chunksize * j;
+    auto stop = (chunksize * (j+1) > Npts) ? Npts : chunksize * (j+1);
+    threads.push_back(std::thread(interp_points, start, stop));
+  }
+  for(auto j = 0; j < nthreads; ++j) {
+    threads[j].join();
+  }
+}
 
 int main(int argc, char **argv) {
-  // // initialize MPI
-  // int rc = MPI_Init(&argc, &argv);
-
-  // if (rc != MPI_SUCCESS) {
-  //   cerr << "Error starting MPI" << endl;
-  //   MPI_Abort(MPI_COMM_WORLD, rc);
-  // }
 
   // expecting a configuration file on execution
   if(argc < 2) {
@@ -51,20 +106,40 @@ int main(int argc, char **argv) {
   }
 
   std::string ifilename{argv[1]};
-  kadath_config_boost<BCO_BH_INFO> bconfig(ifilename);
-  using reader_t = Kadath::FUKA_Solvers::CFMS_BH_Reader<decltype(bconfig), Space_adapted_bh>;
-  reader_t reader(ifilename);
 
-  // Space_adapted_bh bh(*reader.get_space().get());
-  auto reader_copy(reader);
+
+  std::vector<double> xx(Npts);
+  std::vector<double> yy(Npts);
+  std::vector<double> zz(Npts);
+
+  fill_coords(xx,yy,zz);
+  auto v = interpolate(ifilename, xx, yy, zz);
+
+
   
-  if(reader.is_export_ready()) {
-    std::cout << "Let's export!\n";
-    auto interp = reader.interpolate_pointwise(1.,1.,1.);
-    std::cout << "Psi(1,1,1) = " << interp[reader_t::XCTS_VARS::XCTS_PSI] << '\n';
-    auto out = reader.export_pointwise(1.,1.,1.);
-    std::cout << "GXX(1,1,1) = " << out[reader_t::OUTPUT_VARS::GXX] << '\n';
-  }
-  // MPI_Finalize();
+  // #pragma omp parallel for
+  // for(auto i = 0; i < Npts; ++i) {
+  //   xx[i]+= i * dx;
+  //   yy[i]+= i * dx;
+  //   zz[i]+= i * dx;
+  // }
+
+  // #pragma omp parallel for firstprivate(reader)
+  // for(auto i = 0; i < Npts; ++i) {
+  //   reader_t r(reader);
+
+  //   // r = reader;
+  //   // #pragma omp critical 
+  //   // {
+  //   //   r = reader;
+  //   // std::cout << i << "\n";
+  //   std::cout << r.get_space() << "\n";
+  //   auto interp = r.interpolate_pointwise(xx[i], yy[i], zz[i]);
+  //   // std::cout << "Psi(" << xx[i] << ", " << yy[i] << ", " << zz[i] << ") = " << interp[reader_t::XCTS_VARS::XCTS_PSI] << '\n';
+    
+    
+
+  // }
+
   return EXIT_SUCCESS;
 }
