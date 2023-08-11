b4bd6ac36869935308bdb668ef3ebcd970066cd7
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-08-11 14:34:01 +0200

    Add array and pointwise export

--- include/Solvers/reader.hpp
+++ include/Solvers/reader.hpp
@@ -108,16 +108,20 @@ struct CFMS_BH_Reader : public Reader<config_t, space_t> {
   using Reader<config_t, space_t>::syst;
   using Reader<config_t, space_t>::ndom;
 
-  // CFMS_BH specific fields
+  // CFMS_BH imported fields from file
   ptr_data_member(Scalar, conformal_factor, unique);
   ptr_data_member(Scalar, lapse, unique);
   ptr_data_member(Vector, shift, unique);
+
+  // Constructed objects
   ptr_data_member(Base_tensor, basis, unique);
   ptr_data_member(Metric_flat, fmet, unique);
+  ptr_data_member(Tensor, A, unique);
 
   protected:
   std::vector<std::reference_wrapper<const Scalar>> quants;
   bool export_ready{false};
+  int const ndim{3};
 
   void load_solution_from_file() override {
     std::string spacein{bconfig->space_filename()};
@@ -146,7 +150,7 @@ struct CFMS_BH_Reader : public Reader<config_t, space_t> {
 
     // syst->add_def("Ts^i = N * bet^i");
     syst->add_def("A_ij = (D_i bet_j + D_j bet_i - 2. / 3.* D^k bet_k * f_ij) /2. / N");
-    Tensor A(syst->give_val_def("A"));
+    A.reset(new Tensor(syst->give_val_def("A")));
 
     quants[XCTS_VARS::XCTS_PSI] = std::cref(*conformal_factor);
     quants[XCTS_VARS::XCTS_ALPHA] = std::cref(*lapse);
@@ -160,7 +164,7 @@ struct CFMS_BH_Reader : public Reader<config_t, space_t> {
       XCTS_VARS::XCTS_AXZ, 
       XCTS_VARS::XCTS_AYY, 
       XCTS_VARS::XCTS_AYZ, 
-      XCTS_VARS::XCTS_AZZ}, A);
+      XCTS_VARS::XCTS_AZZ}, *A);
     export_ready = true;
   }
 
@@ -197,11 +201,144 @@ struct CFMS_BH_Reader : public Reader<config_t, space_t> {
   
 
   public:
-  std::array<std::vector<double>, nout> export_array(int const npoints, double const * xx, double const * yy, double const * zz,
-    double const interpolation_offset, int const interp_order, double const delta_r_rel) {
 
-    //sdf
+  std::vector<double> interpolate_pointwise(double const & x, double const & y, double const & z,
+    double const interpolation_offset = 0., int const interp_order = 8, double const delta_r_rel = 0.3) {
+    
+    std::vector<double> quant_vals(XCTS_VARS::NUM_XCTS_VARS);
 
+    // Initial guess of the excision radius - needed for filling
+    double rbh = bco_utils::get_radius(space->get_domain(1), INNER_BC);
+
+    double r2yz = y * y + z * z;
+
+    double r = std::sqrt(x * x + r2yz);
+
+    // lambda function for filling excised region
+    auto interp_f = [&](auto& ah_r, auto& extrap_r, auto bh_ori) {
+      // Avoid division by "0"
+      extrap_r = (extrap_r <= 1e-12) ? 1e-12 : extrap_r;
+      double x_shifted = (x == 0.) ? 1e-14 : x;
+      double theta = std::acos(z / extrap_r);
+      
+      // atan2 is needed here
+      double phi = std::atan2(y, (x_shifted - bh_ori)); 
+
+      // Where the filling takes places
+      export_utils::spherical_turduck(
+        quants, quant_vals, interp_order, delta_r_rel, interpolation_offset, 
+        rbh, extrap_r, theta, phi, 1, bh_ori
+      );
+    };
+
+    if (r <= (1. + interpolation_offset) * rbh) {
+      interp_f(rbh, r, 0.);
+    } else { 
+      Point abs_coords(ndim);
+      abs_coords.set(1) = x;
+      abs_coords.set(2) = y;
+      abs_coords.set(3) = z;
+
+      for (int k = 0; k < XCTS_VARS::NUM_XCTS_VARS; ++k) {
+        quant_vals[k] = quants[k].get().val_point(abs_coords);
+      }
+    }
+    return quant_vals;
+  }
+
+  std::array<std::vector<double>, OUTPUT_VARS::NUM_OUTPUT_VARS> export_coordinate_array(
+    int const npoints, double const * xx, double const * yy, double const * zz,
+    double const interpolation_offset = 0., int const interp_order = 8, double const delta_r_rel = 0.3) {
+    std::array<std::vector<double>,OUTPUT_VARS::NUM_OUTPUT_VARS> out;
+    for(auto& v : out)
+      v.resize(npoints);
+    
+    for (int i = 0; i < npoints; ++i) {
+      
+      auto quant_vals = interpolate_pointwise(xx[i], yy[i], zz[i], interpolation_offset, interp_order, delta_r_rel);
+      
+      // Fill output vector by storing non-conformal quantities
+      auto const psi = quant_vals[XCTS_VARS::XCTS_PSI];
+      auto const psi2 = psi * psi;
+      auto const psi4 = psi2 * psi2;
+
+      out[OUTPUT_VARS::ALPHA][i] = quant_vals[XCTS_VARS::XCTS_ALPHA];
+
+      out[OUTPUT_VARS::BETAX][i] = quant_vals[XCTS_VARS::XCTS_BETAX];
+      out[OUTPUT_VARS::BETAY][i] = quant_vals[XCTS_VARS::XCTS_BETAY];
+      out[OUTPUT_VARS::BETAZ][i] = quant_vals[XCTS_VARS::XCTS_BETAZ];
+
+      double g[3][3];
+      g[0][0] = psi4;
+      g[0][1] = 0.0;
+      g[0][2] = 0.0;
+      g[1][1] = psi4;
+      g[1][2] = 0.0;
+      g[2][2] = psi4;
+      g[1][0] = g[0][1];
+      g[2][0] = g[0][2];
+      g[2][1] = g[1][2];
+
+      out[OUTPUT_VARS::GXX][i] = g[0][0];
+      out[OUTPUT_VARS::GXY][i] = g[0][1];
+      out[OUTPUT_VARS::GXZ][i] = g[0][2];
+      out[OUTPUT_VARS::GYY][i] = g[1][1];
+      out[OUTPUT_VARS::GYZ][i] = g[1][2];
+      out[OUTPUT_VARS::GZZ][i] = g[2][2];
+
+      out[OUTPUT_VARS::KXX][i] = quant_vals[XCTS_VARS::XCTS_AXX] * psi4;
+      out[OUTPUT_VARS::KXY][i] = quant_vals[XCTS_VARS::XCTS_AXY] * psi4;
+      out[OUTPUT_VARS::KXZ][i] = quant_vals[XCTS_VARS::XCTS_AXZ] * psi4;
+      out[OUTPUT_VARS::KYY][i] = quant_vals[XCTS_VARS::XCTS_AYY] * psi4;
+      out[OUTPUT_VARS::KYZ][i] = quant_vals[XCTS_VARS::XCTS_AYZ] * psi4;
+      out[OUTPUT_VARS::KZZ][i] = quant_vals[XCTS_VARS::XCTS_AZZ] * psi4;
+    }
+  }
+  
+  std::array<double, OUTPUT_VARS::NUM_OUTPUT_VARS> export_pointwise(
+    double const & x, double const & y, double const & z,
+    double const interpolation_offset = 0., int const interp_order = 8, double const delta_r_rel = 0.3) {
+    
+    std::array<double, OUTPUT_VARS::NUM_OUTPUT_VARS> out;
+      
+    auto quant_vals = interpolate_pointwise(x, y, z, interpolation_offset, interp_order, delta_r_rel);
+    
+    // Fill output vector by storing non-conformal quantities
+    auto const psi = quant_vals[XCTS_VARS::XCTS_PSI];
+    auto const psi2 = psi * psi;
+    auto const psi4 = psi2 * psi2;
+
+    out[OUTPUT_VARS::ALPHA] = quant_vals[XCTS_VARS::XCTS_ALPHA];
+
+    out[OUTPUT_VARS::BETAX] = quant_vals[XCTS_VARS::XCTS_BETAX];
+    out[OUTPUT_VARS::BETAY] = quant_vals[XCTS_VARS::XCTS_BETAY];
+    out[OUTPUT_VARS::BETAZ] = quant_vals[XCTS_VARS::XCTS_BETAZ];
+
+    double g[3][3];
+    g[0][0] = psi4;
+    g[0][1] = 0.0;
+    g[0][2] = 0.0;
+    g[1][1] = psi4;
+    g[1][2] = 0.0;
+    g[2][2] = psi4;
+    g[1][0] = g[0][1];
+    g[2][0] = g[0][2];
+    g[2][1] = g[1][2];
+
+    out[OUTPUT_VARS::GXX] = g[0][0];
+    out[OUTPUT_VARS::GXY] = g[0][1];
+    out[OUTPUT_VARS::GXZ] = g[0][2];
+    out[OUTPUT_VARS::GYY] = g[1][1];
+    out[OUTPUT_VARS::GYZ] = g[1][2];
+    out[OUTPUT_VARS::GZZ] = g[2][2];
+
+    out[OUTPUT_VARS::KXX] = quant_vals[XCTS_VARS::XCTS_AXX] * psi4;
+    out[OUTPUT_VARS::KXY] = quant_vals[XCTS_VARS::XCTS_AXY] * psi4;
+    out[OUTPUT_VARS::KXZ] = quant_vals[XCTS_VARS::XCTS_AXZ] * psi4;
+    out[OUTPUT_VARS::KYY] = quant_vals[XCTS_VARS::XCTS_AYY] * psi4;
+    out[OUTPUT_VARS::KYZ] = quant_vals[XCTS_VARS::XCTS_AYZ] * psi4;
+    out[OUTPUT_VARS::KZZ] = quant_vals[XCTS_VARS::XCTS_AZZ] * psi4;
+    return out;
   }
 };
 }
