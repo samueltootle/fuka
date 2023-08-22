44f47adeb46a1db8c23f6de98cfb8abfefeb89cc
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-08-22 09:41:39 +0200

    ensure resolution is initialized in the Config

--- include/Solvers/ns_isotropic/ns_isotropic_driver_imp.cpp
+++ include/Solvers/ns_isotropic/ns_isotropic_driver_imp.cpp
@@ -230,6 +230,7 @@ inline int ns_isotropic_driver (config_t& bconfig, Res_t& resolution, std::strin
   bool res_inc = (resolution.final() > resolution.init());
   auto resolution_indices = resolution.get_indices();
   auto const & final_res = resolution.final();
+  bconfig.set(resolution_indices) = resolution.init();
   
   std::array<bool, NUM_STAGES>& stage_enabled = bconfig.return_stages();
   auto [ last_stage, last_stage_idx ] = get_last_enabled(MSTAGE, stage_enabled);
