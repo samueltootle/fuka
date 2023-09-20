e5a8191e3f00860cf1bb03d28c809eb519b59ceb
S.Tootle <tootle@itp.uni-frankfurt.de>
2023-09-20 14:14:25 +0200

    Add gargbage resolution loops Add additional alternate plots

--- codes/FUKAv2_Solvers/NS_isotropic/parse.py
+++ codes/FUKAv2_Solvers/NS_isotropic/parse.py
@@ -12,41 +12,45 @@ import glob
 
 if __name__ == "__main__":
   print("test", glob.glob("res_seq*"))
-  for d in sorted(glob.glob("res_seq*")):
-    fabs=d+"/NS_ISO_NOROT_BC.togashi.hc.1.32632.0.0.*.info"
-    print(fabs)
-    files = glob.glob(fabs)
-#    print(files)
-    rho = []
-    Mb = []
-    res = []
-    res_r = []
-    Madm = []
-    Mk = []
-    for f in sorted(files):
-      fID, p = check_ID_filename(f)
-      ns = get_reader(fID, ns_iso_norot=True)
-      if not ns.config['ns']['res'] in [9, 11, 13, 17, 21, 25, 33]:
-        continue
-      # print(ns.vars['nc'], ns.vars['Mb'])
-      rho.append(ns.vars['nc'] * 6.17714e17)
-      Mb.append(ns.vars['Mb'])
-      Madm.append(ns.vars['Madm'])
-      Mk.append(ns.vars['Mk'])
-      res_sum = 0
-      for reslist in ns.vars['domain_resolutions']:
-        res_sum += reslist[0] * reslist[1]
-      res.append(res_sum)
-      res_r.append(ns.config['ns']['res'])
-    # plt.plot([r**(1./2.) for r in res[:-1]], [math.fabs(1. - M / Madm[-1]) for M in Madm[:-1]], label=d)
-    plt.plot([r**(1./2.) for r in res], [math.fabs(1. - mk / madm) for mk, madm in zip(Mk, Madm)], label=d)
-    print(len(rho))
-  # plt.xlabel(r'$\rho {\rm [g / {cm}^3]}$')
-  # plt.ylabel(r'$M_b {\rm [M_\odot]}$')
-  plt.xlabel(r'$\bar{N}^{1/2}$')
-  plt.ylabel(r'$|1 - M^{\bar{N}}_{\rm ADM} / M^{\bar{N}, HR}_{\rm ADM}|$')
-  plt.yscale('log')
+  reslist=[13] #, 17, 21, 25, 33]
+  for R in reslist:
+    for d in sorted(glob.glob("seq_rr*A-1")):
+      fabs=d+"/NS_ISO_DIFF_ROT.gam2.keh.nc.*.info"
+      print(fabs)
+      files = glob.glob(fabs)
+  #    print(files)
+      rho = []
+      Mb = []
+      res = []
+      res_r = []
+      Madm = []
+      Mk = []
+      for f in sorted(files):
+        fID, p = check_ID_filename(f)
+        ns = get_reader(fID, ns_iso_diffrot=True)
+        # print(ns.config['ns']['res'])
+        if not ns.config['ns']['res'] == int(R) :
+          continue
+        # print(ns.vars['nc'], ns.vars['Mb'])
+        rho.append(ns.vars['nc']) # * 6.17714e17)
+        Mb.append(ns.vars['Mb'])
+        Madm.append(ns.vars['Madm'])
+        Mk.append(ns.vars['Mk'])
+        res_sum = 0
+        for reslist in ns.vars['domain_resolutions']:
+          res_sum += reslist[0] * reslist[1]
+        res.append(res_sum)
+        res_r.append(ns.config['ns']['res'])
+      plt.plot(rho, Madm, label=d)
+      # plt.plot([r**(1./2.) for r in res[:-1]], [math.fabs(1. - M / Madm[-1]) for M in Madm[:-1]], label=d)
+      # plt.plot([r**(1./2.) for r in res], [math.fabs(1. - mk / madm) for mk, madm in zip(Mk, Madm)], label=d)
+      print(len(rho))
+  plt.xlabel(r'$\rho {\rm [g / {cm}^3]}$')
+  plt.ylabel(r'$M_b {\rm [M_\odot]}$')
+  plt.ylim(-0.1, 2.6)
+  plt.legend()
+  # plt.xlabel(r'$\bar{N}^{1/2}$')
+  # plt.ylabel(r'$|1 - M^{\bar{N}}_{\rm ADM} / M^{\bar{N}, HR}_{\rm ADM}|$')
+  # plt.yscale('log')
+  plt.minorticks_on()
   plt.savefig('seq1.pdf')
-  # args = get_args()
-  # f, ispickle = check_ID_filename(args.filename)
-  # print("Reading from file: {}".format(f))
