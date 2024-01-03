b5f4b5db15c91c6d4e9db72cb73b7a0576c37676
S.Tootle <sdtootle@gmail.com>
2024-01-02 17:19:00 -0700

    move stream operator definitions

--- include/codes_utilities.hpp
+++ include/codes_utilities.hpp
@@ -121,7 +121,7 @@ struct Option_base {
         }
     }
 };
-std::ostream & operator<<(std::ostream & os,Option_base const & option) {option.display(os); return os;}
+//std::ostream & operator<<(std::ostream & os,Option_base const & option) {option.display(os); return os;}
 
 template<typename T> struct Option : Option_base {
     T value;
@@ -245,10 +245,10 @@ public:
         }
     }
 };
-std::ostream& operator<<(std::ostream& os,Arguments_parser const & arguments_parser) {
-    arguments_parser.display(os);
-    return os;
-}
+// std::ostream& operator<<(std::ostream& os,Arguments_parser const & arguments_parser) {
+//     arguments_parser.display(os);
+//     return os;
+// }
 
 
 
