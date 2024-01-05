37a7075b0373a0cdeab657254dce48386b61c8cd
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2024-01-05 08:30:39 -0800

    current

--- include/memory.hpp
+++ include/memory.hpp
@@ -33,7 +33,7 @@
 // only do this if really necessary, e.g. using an Intel compiler not capable of compiling the flat_hash_map below
 // this is slower than the hash map
 // #define KADATH_VECTORMAP
-// #define DEFAULT_KAD_MEM
+#define DEFAULT_KAD_MEM
 #ifndef KADATH_VECTORMAP
 #include "implementation/flat_hash_map.hpp"
 #endif
