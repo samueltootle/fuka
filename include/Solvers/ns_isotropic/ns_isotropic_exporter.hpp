afb79618fa47c423a76ebcf94a463d1ab29d8d0c
s.tootle <sdtootle@gmail.com>
2024-01-10 17:15:53 -0800

    Add fluid velocity to codegen

--- include/Solvers/ns_isotropic/ns_isotropic_exporter.hpp
+++ include/Solvers/ns_isotropic/ns_isotropic_exporter.hpp
@@ -80,10 +80,6 @@ struct CFMS_NS_ISO_Exporter : public Exporter<Kadath::FUKA_Config::kadath_config
 
   // Constructed objects
   // ptr_data_member(Tensor, A, shared);
-  
-  // dphi = (-y, +x, 0)
-  ptr_data_member(Scalar, X, shared);
-  ptr_data_member(Scalar, Y, shared);
 
   ptr_data_member(Scalar, metric_A, shared);
   ptr_data_member(Scalar, metric_B, shared);
@@ -305,12 +301,18 @@ struct CFMS_NS_ISO_Exporter : public Exporter<Kadath::FUKA_Config::kadath_config
       }
       // Unpack initial_data for ADM vectors/tensors
       const double N = quant_vals[ISO_VARS::ISO_ALPHA];
+      double const omega = quant_vals[ISO_VARS::ISO_OMEGA];
+      const double Omega = (*bconfig)(BCO_PARAMS::OMEGA);
+      const double U_factor = (Omega - omega) / N * xx0 * sin(xx2);
       const double domega_dr = quant_vals[ISO_VARS::ISO_DOMEGA_DR];
       const double domega_dt = quant_vals[ISO_VARS::ISO_DOMEGA_DTHETA];
 
-      const double betaSpherical0 = 0.0;                              // r
-      const double betaSpherical1 = 0.0;                              // theta
-      const double betaSpherical2 = -quant_vals[ISO_VARS::ISO_OMEGA]; // phi
+      const double FluidVelU0 = 0.0;                                   // r
+      const double FluidVelU1 = 0.0;                                   // theta
+      const double FluidVelU2 = U_factor;                              // phi
+      const double betaSphericalU0 = 0.0;                              // r
+      const double betaSphericalU1 = 0.0;                              // theta
+      const double betaSphericalU2 = -quant_vals[ISO_VARS::ISO_OMEGA]; // phi
 
       const double A = quant_vals[ISO_VARS::ISO_METRIC_A];
       const double B = quant_vals[ISO_VARS::ISO_METRIC_B];
@@ -330,89 +332,90 @@ struct CFMS_NS_ISO_Exporter : public Exporter<Kadath::FUKA_Config::kadath_config
       const double KSphericalDD12 = -gammaSphericalDD22 / 2.0 / N * domega_dt;
       const REAL tmp0 = cos(xx2);
       const REAL tmp1 = sin(xx1);
-      const REAL tmp3 = cos(xx1);
+      const REAL tmp4 = cos(xx1);
       const REAL tmp6 = sin(xx2);
       const REAL tmp12 = ((xx0) * (xx0));
-      const REAL tmp5 = betaSpherical1 * tmp3 * xx0;
-      const REAL tmp7 = tmp6 * xx0;
-      const REAL tmp9 = tmp0 * xx0;
+      const REAL tmp3 = tmp0 * xx0;
+      const REAL tmp7 = tmp1 * xx0;
+      const REAL tmp9 = tmp6 * xx0;
       const REAL tmp10 = ((tmp0) * (tmp0));
       const REAL tmp11 = ((tmp6) * (tmp6));
       const REAL tmp13 = ((tmp1) * (tmp1) * (tmp1));
-      const REAL tmp15 = ((tmp3) * (tmp3));
+      const REAL tmp15 = ((tmp4) * (tmp4));
       const REAL tmp20 = ((tmp1) * (tmp1) * (tmp1) * (tmp1)) * ((xx0) * (xx0) * (xx0) * (xx0));
       const REAL tmp25 = ((tmp1) * (tmp1));
-      const REAL tmp33 = tmp1 * tmp3;
       const REAL tmp17 = tmp1 * tmp12 * tmp15;
       const REAL tmp21 = gammaSphericalDD00 * tmp20;
-      const REAL tmp23 = tmp13 * tmp3 * ((xx0) * (xx0) * (xx0));
+      const REAL tmp23 = tmp13 * tmp4 * ((xx0) * (xx0) * (xx0));
       const REAL tmp26 = tmp12 * tmp25;
-      const REAL tmp29 = -tmp15 * tmp7 - tmp25 * tmp7;
-      const REAL tmp34 = gammaSphericalDD12 * tmp33;
-      const REAL tmp41 = tmp15 * tmp9 + tmp25 * tmp9;
-      const REAL tmp48 = tmp33 * tmp9;
-      const REAL tmp55 = tmp1 * tmp12 * tmp3;
-      const REAL tmp66 = tmp33 * tmp7;
-      const REAL tmp71 = KSphericalDD00 * tmp20;
+      const REAL tmp29 = -tmp15 * tmp9 - tmp25 * tmp9;
+      const REAL tmp34 = tmp4 * tmp7;
+      const REAL tmp41 = tmp15 * tmp3 + tmp25 * tmp3;
+      const REAL tmp52 = tmp1 * tmp12 * tmp4;
+      const REAL tmp68 = KSphericalDD00 * tmp20;
       const REAL tmp18 = (1.0 / ((tmp10 * tmp12 * tmp13 + tmp10 * tmp17 + tmp11 * tmp12 * tmp13 + tmp11 * tmp17) *
                                 (tmp10 * tmp12 * tmp13 + tmp10 * tmp17 + tmp11 * tmp12 * tmp13 + tmp11 * tmp17)));
       const REAL tmp24 = 2 * gammaSphericalDD01 * tmp23;
-      const REAL tmp36 = gammaSphericalDD02 * tmp26;
-      const REAL tmp50 = -tmp10 * tmp25 * xx0 - tmp11 * tmp25 * xx0;
-      const REAL tmp56 = tmp10 * tmp55 + tmp11 * tmp55;
-      const REAL tmp73 = 2 * KSphericalDD01 * tmp23;
-      const REAL tmp77 = KSphericalDD02 * tmp26;
+      const REAL tmp35 = gammaSphericalDD12 * tmp34;
+      const REAL tmp37 = gammaSphericalDD02 * tmp26;
+      const REAL tmp47 = -tmp10 * tmp25 * xx0 - tmp11 * tmp25 * xx0;
+      const REAL tmp53 = tmp10 * tmp52 + tmp11 * tmp52;
+      const REAL tmp70 = 2 * KSphericalDD01 * tmp23;
+      const REAL tmp73 = KSphericalDD12 * tmp34;
+      const REAL tmp75 = KSphericalDD02 * tmp26;
       const REAL tmp19 = tmp10 * tmp18;
       const REAL tmp28 = gammaSphericalDD11 * tmp15 * tmp26;
       const REAL tmp31 = gammaSphericalDD22 * tmp18;
       const REAL tmp32 = tmp18 * tmp29;
       const REAL tmp39 = tmp0 * tmp18;
       const REAL tmp42 = tmp18 * tmp41;
-      const REAL tmp57 = tmp18 * tmp56;
-      const REAL tmp63 = tmp11 * tmp18;
-      const REAL tmp68 = tmp18 * ((tmp50) * (tmp50));
-      const REAL tmp69 = tmp18 * ((tmp56) * (tmp56));
-      const REAL tmp74 = KSphericalDD11 * tmp15 * tmp26;
-      const REAL tmp75 = KSphericalDD22 * tmp18;
+      const REAL tmp54 = tmp18 * tmp53;
+      const REAL tmp61 = tmp11 * tmp18;
+      const REAL tmp64 = tmp18 * tmp6;
+      const REAL tmp65 = tmp18 * ((tmp47) * (tmp47));
+      const REAL tmp66 = tmp18 * ((tmp53) * (tmp53));
+      const REAL tmp71 = KSphericalDD11 * tmp15 * tmp26;
+      const REAL tmp72 = KSphericalDD22 * tmp18;
+      const REAL tmp33 = tmp0 * tmp32;
       const REAL tmp40 = tmp39 * tmp6;
-      const REAL tmp52 = gammaSphericalDD11 * tmp18 * tmp50;
-      const REAL tmp54 = gammaSphericalDD01 * tmp26 * tmp50;
-      const REAL tmp80 = KSphericalDD11 * tmp18 * tmp50;
-      const REAL tmp81 = KSphericalDD01 * tmp26 * tmp50;
-      const REAL tmp60 = gammaSphericalDD00 * tmp26 * tmp57;
-      const REAL tmp83 = KSphericalDD00 * tmp26 * tmp57;
-      out_pw[OUTPUT_VARS::BETAX] = betaSpherical0 * tmp0 * tmp1 - betaSpherical2 * tmp1 * tmp7 + tmp0 * tmp5;
-      out_pw[OUTPUT_VARS::BETAY] = betaSpherical0 * tmp1 * tmp6 + betaSpherical2 * tmp1 * tmp9 + tmp5 * tmp6;
-      out_pw[OUTPUT_VARS::BETAZ] = betaSpherical0 * tmp3 - betaSpherical1 * tmp1 * xx0;
-      out_pw[OUTPUT_VARS::GXX] =
-          2 * tmp0 * tmp32 * tmp36 + tmp19 * tmp21 + tmp19 * tmp24 + tmp19 * tmp28 + ((tmp29) * (tmp29)) * tmp31 + 2 * tmp32 * tmp34 * tmp9;
-      out_pw[OUTPUT_VARS::GXY] = tmp0 * tmp36 * tmp42 + tmp21 * tmp40 + tmp24 * tmp40 + tmp28 * tmp40 + tmp29 * tmp31 * tmp41 + tmp32 * tmp34 * tmp7 +
-                                tmp32 * tmp36 * tmp6 + tmp34 * tmp42 * tmp9;
-      out_pw[OUTPUT_VARS::GXZ] = gammaSphericalDD01 * tmp48 * tmp57 + gammaSphericalDD02 * tmp32 * tmp56 + gammaSphericalDD12 * tmp32 * tmp50 +
-                                tmp0 * tmp60 + tmp39 * tmp54 + tmp48 * tmp52;
-      out_pw[OUTPUT_VARS::GYY] =
-          tmp21 * tmp63 + tmp24 * tmp63 + tmp28 * tmp63 + tmp31 * ((tmp41) * (tmp41)) + 2 * tmp34 * tmp42 * tmp7 + 2 * tmp36 * tmp42 * tmp6;
-      out_pw[OUTPUT_VARS::GYZ] = gammaSphericalDD01 * tmp57 * tmp66 + gammaSphericalDD02 * tmp42 * tmp56 + gammaSphericalDD12 * tmp42 * tmp50 +
-                                tmp18 * tmp54 * tmp6 + tmp52 * tmp66 + tmp6 * tmp60;
-      out_pw[OUTPUT_VARS::GZZ] = gammaSphericalDD00 * tmp69 + 2 * gammaSphericalDD01 * tmp50 * tmp57 + gammaSphericalDD11 * tmp68;
-      out_pw[OUTPUT_VARS::KXX] =
-          2 * KSphericalDD12 * tmp32 * tmp48 + 2 * tmp0 * tmp32 * tmp77 + tmp19 * tmp71 + tmp19 * tmp73 + tmp19 * tmp74 + ((tmp29) * (tmp29)) * tmp75;
-      out_pw[OUTPUT_VARS::KXY] = KSphericalDD12 * tmp32 * tmp33 * tmp7 + KSphericalDD12 * tmp33 * tmp42 * tmp9 + tmp0 * tmp42 * tmp77 +
-                                tmp29 * tmp41 * tmp75 + tmp32 * tmp6 * tmp77 + tmp40 * tmp71 + tmp40 * tmp73 + tmp40 * tmp74;
+      const REAL tmp43 = tmp0 * tmp42;
+      const REAL tmp44 = tmp32 * tmp6;
+      const REAL tmp49 = gammaSphericalDD11 * tmp34 * tmp47;
+      const REAL tmp51 = gammaSphericalDD01 * tmp26 * tmp47;
+      const REAL tmp63 = tmp42 * tmp6;
+      const REAL tmp77 = KSphericalDD11 * tmp34 * tmp47;
+      const REAL tmp78 = KSphericalDD01 * tmp26 * tmp47;
+      const REAL tmp56 = gammaSphericalDD01 * tmp34 * tmp54;
+      const REAL tmp58 = gammaSphericalDD00 * tmp26 * tmp54;
+      const REAL tmp79 = KSphericalDD01 * tmp34 * tmp54;
+      const REAL tmp80 = KSphericalDD00 * tmp26 * tmp54;
+      out_pw[OUTPUT_VARS::BETAX] = betaSphericalU0 * tmp0 * tmp1 + betaSphericalU1 * tmp3 * tmp4 - betaSphericalU2 * tmp6 * tmp7;
+      out_pw[OUTPUT_VARS::BETAY] = betaSphericalU0 * tmp1 * tmp6 + betaSphericalU1 * tmp4 * tmp9 + betaSphericalU2 * tmp0 * tmp7;
+      out_pw[OUTPUT_VARS::BETAZ] = betaSphericalU0 * tmp4 - betaSphericalU1 * tmp7;
+      out_pw[OUTPUT_VARS::GXX] = tmp19 * tmp21 + tmp19 * tmp24 + tmp19 * tmp28 + ((tmp29) * (tmp29)) * tmp31 + 2 * tmp33 * tmp35 + 2 * tmp33 * tmp37;
+      out_pw[OUTPUT_VARS::GXY] =
+          tmp21 * tmp40 + tmp24 * tmp40 + tmp28 * tmp40 + tmp29 * tmp31 * tmp41 + tmp35 * tmp43 + tmp35 * tmp44 + tmp37 * tmp43 + tmp37 * tmp44;
+      out_pw[OUTPUT_VARS::GXZ] =
+          gammaSphericalDD02 * tmp32 * tmp53 + gammaSphericalDD12 * tmp32 * tmp47 + tmp0 * tmp56 + tmp0 * tmp58 + tmp39 * tmp49 + tmp39 * tmp51;
+      out_pw[OUTPUT_VARS::GYY] = tmp21 * tmp61 + tmp24 * tmp61 + tmp28 * tmp61 + tmp31 * ((tmp41) * (tmp41)) + 2 * tmp35 * tmp63 + 2 * tmp37 * tmp63;
+      out_pw[OUTPUT_VARS::GYZ] =
+          gammaSphericalDD02 * tmp42 * tmp53 + gammaSphericalDD12 * tmp42 * tmp47 + tmp49 * tmp64 + tmp51 * tmp64 + tmp56 * tmp6 + tmp58 * tmp6;
+      out_pw[OUTPUT_VARS::GZZ] = gammaSphericalDD00 * tmp66 + 2 * gammaSphericalDD01 * tmp47 * tmp54 + gammaSphericalDD11 * tmp65;
+      out_pw[OUTPUT_VARS::KXX] = tmp19 * tmp68 + tmp19 * tmp70 + tmp19 * tmp71 + ((tmp29) * (tmp29)) * tmp72 + 2 * tmp33 * tmp73 + 2 * tmp33 * tmp75;
+      out_pw[OUTPUT_VARS::KXY] =
+          tmp29 * tmp41 * tmp72 + tmp40 * tmp68 + tmp40 * tmp70 + tmp40 * tmp71 + tmp43 * tmp73 + tmp43 * tmp75 + tmp44 * tmp73 + tmp44 * tmp75;
       out_pw[OUTPUT_VARS::KXZ] =
-          KSphericalDD01 * tmp48 * tmp57 + KSphericalDD02 * tmp32 * tmp56 + KSphericalDD12 * tmp32 * tmp50 + tmp0 * tmp83 + tmp39 * tmp81 + tmp48 * tmp80;
-      out_pw[OUTPUT_VARS::KYY] =
-          2 * KSphericalDD12 * tmp42 * tmp66 + ((tmp41) * (tmp41)) * tmp75 + 2 * tmp42 * tmp6 * tmp77 + tmp63 * tmp71 + tmp63 * tmp73 + tmp63 * tmp74;
-      out_pw[OUTPUT_VARS::KYZ] = KSphericalDD01 * tmp57 * tmp66 + KSphericalDD02 * tmp42 * tmp56 + KSphericalDD12 * tmp42 * tmp50 + tmp18 * tmp6 * tmp81 +
-                                tmp6 * tmp83 + tmp66 * tmp80;
-      out_pw[OUTPUT_VARS::KZZ] = KSphericalDD00 * tmp69 + 2 * KSphericalDD01 * tmp50 * tmp57 + KSphericalDD11 * tmp68;
+          KSphericalDD02 * tmp32 * tmp53 + KSphericalDD12 * tmp32 * tmp47 + tmp0 * tmp79 + tmp0 * tmp80 + tmp39 * tmp77 + tmp39 * tmp78;
+      out_pw[OUTPUT_VARS::KYY] = ((tmp41) * (tmp41)) * tmp72 + tmp61 * tmp68 + tmp61 * tmp70 + tmp61 * tmp71 + 2 * tmp63 * tmp73 + 2 * tmp63 * tmp75;
+      out_pw[OUTPUT_VARS::KYZ] =
+          KSphericalDD02 * tmp42 * tmp53 + KSphericalDD12 * tmp42 * tmp47 + tmp6 * tmp79 + tmp6 * tmp80 + tmp64 * tmp77 + tmp64 * tmp78;
+      out_pw[OUTPUT_VARS::KZZ] = KSphericalDD00 * tmp66 + 2 * KSphericalDD01 * tmp47 * tmp54 + KSphericalDD11 * tmp65;
+      out_pw[OUTPUT_VARS::VELX] = FluidVelU0 * tmp0 * tmp1 + FluidVelU1 * tmp3 * tmp4 - FluidVelU2 * tmp6 * tmp7;
+      out_pw[OUTPUT_VARS::VELY] = FluidVelU0 * tmp1 * tmp6 + FluidVelU1 * tmp4 * tmp9 + FluidVelU2 * tmp0 * tmp7;
+      out_pw[OUTPUT_VARS::VELZ] = FluidVelU0 * tmp4 - FluidVelU1 * tmp7;
     };
 
     double const N = quant_vals[ISO_VARS::ISO_ALPHA];
-    double const omega = quant_vals[ISO_VARS::ISO_OMEGA];
-    const double Omega = (*bconfig)(BCO_PARAMS::OMEGA);
-    const double U_factor = (Omega - omega) / N;
-
     double const H = quant_vals[ISO_VARS::ISO_H];
     double h = std::exp(H);
     double rho, eps, press;
@@ -432,9 +435,6 @@ struct CFMS_NS_ISO_Exporter : public Exporter<Kadath::FUKA_Config::kadath_config
     out_pw[OUTPUT_VARS::RHO]   = rho;
     out_pw[OUTPUT_VARS::EPS]   = eps;
     out_pw[OUTPUT_VARS::PRESS] = press;
-    out_pw[OUTPUT_VARS::VELX]  = - quant_vals[ISO_VARS::ISO_CARTY] * U_factor;
-    out_pw[OUTPUT_VARS::VELY]  =   quant_vals[ISO_VARS::ISO_CARTX] * U_factor;
-    out_pw[OUTPUT_VARS::VELZ]  = 0.;
     return out_pw;
   }
 
