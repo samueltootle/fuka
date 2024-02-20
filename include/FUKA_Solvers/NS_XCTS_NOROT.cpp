57d352055f87ff74e1c81c6ef8c75a78b1e2da0e
s.tootle <sdtootle@gmail.com>
2024-02-20 12:52:42 -0800

    Add regrid Add increment_seq Add reset ptrs

--- include/FUKA_Solvers/NS_XCTS_NOROT.cpp
+++ include/FUKA_Solvers/NS_XCTS_NOROT.cpp
@@ -326,6 +326,25 @@ namespace Kadath::FUKA_Solvers {
   void NS_XCTS_NOROT<eos_t>::save_to_file() const {
     Kadath::bco_utils::save_to_file(*space, *bconfig, *conformal_factor, *lapse, *shift, *logh);
   }
+  
+  template<class eos_t>
+  void NS_XCTS_NOROT<eos_t>::reset_all_ptrs() {
+    // Fields
+    conformal_factor.reset(nullptr) ;
+    lapse.reset(nullptr);
+    shift.reset(nullptr);
+    logh.reset(nullptr);
+
+    // Containers
+    basis.reset(nullptr);
+    fmet.reset(nullptr);
+    syst.reset(nullptr);
+    cfields.reset(nullptr);
+    coord_vectors.reset(nullptr);
+
+    // Space
+    space.reset(nullptr);
+  }
 
   template<class eos_t>
   void NS_XCTS_NOROT<eos_t>::load_solution_from_file() {
@@ -342,38 +361,155 @@ namespace Kadath::FUKA_Solvers {
     ndom = space->get_nbr_domains();
   }
 
-
+  template<class eos_t>
+  bool NS_XCTS_NOROT<eos_t>::increment_seq() {
+    if(!seq->is_set() || last_stage_idx != ::Kadath::FUKA_Config::STAGES::NOROT_BC)
+      return false;
     
-  // template<class config_t>
-  // inline auto initialize_NS_XCTS_NOROT_solver(config_t& bconfig) {
-  //   // Initialize EOS
-  //   auto h_cut    = bconfig.template eos<double>(Kadath::FUKA_Config::EOS_PARAMS::HCUT);
-  //   auto eos_file = bconfig.template eos<std::string>(Kadath::FUKA_Config::EOS_PARAMS::EOSFILE);
-  //   auto eos_type = bconfig.template eos<std::string>(Kadath::FUKA_Config::EOS_PARAMS::EOSTYPE);
+    auto sequence_var_indices = seq->get_indices();
+    auto const & dx = seq->step_size();
+    auto x = bconfig->set(sequence_var_indices) + dx;
+    if(seq->loop_condition(x)) {
+      bconfig->set(sequence_var_indices) = x;
+      return true;
+    }
+    return false;
+  }
+    
+  template<class eos_t>
+  void NS_XCTS_NOROT<eos_t>::regrid() {
+    std::string outputfile{"ns_regrid"};
+    
+    if(rank == 0) {
+    std::cout << "Resolution of old space: "
+      << space->get_domain(0)->get_nbr_points()(0) << " (r), "
+      << space->get_domain(0)->get_nbr_points()(1) << " (theta), "
+      << space->get_domain(0)->get_nbr_points()(2) << " (phi)" << std::endl;
+  
+    int ndim = 3;
+    // get the adapted domain and cast it to its correct type to be able to call its member functions
+    const Domain_shell_outer_adapted* old_outer_adapted =
+        dynamic_cast<const Domain_shell_outer_adapted*>(space->get_domain(1));
+    
+    // setup a scalar field representing the old radius
+    Scalar old_space_radius(*space);
+    old_space_radius = 0.;
 
-  //   if(eos_type == "Cold_Table") {
-  //     using namespace Kadath::Margherita;
-  //     using eos_t = Kadath::Margherita::Cold_Table;
+    // get the radius from each domain
+    for(int i = 0; i < space->get_nbr_domains(); ++i)
+      old_space_radius.set_domain(i) = space->get_domain(i)->get_radius();
+    // get the adapted radius of the adapted domain
+    old_space_radius.set_domain(1) = old_outer_adapted->get_outer_radius();
 
-  //     const int interp_pts = (bconfig.template eos<int>(Kadath::FUKA_Config::EOS_PARAMS::INTERP_PTS) == 0) ? \
-  //                             2000 : (*bconfig.template eos<int>(Kadath::FUKA_Config::EOS_PARAMS::INTERP_PTS);
+    // define a standard decomposition, compatible with the parity of this field
+    old_space_radius.std_base();
+    //end setup old radius field
 
-  //     EOS<eos_t,PRESSURE>::init(eos_file, h_cut, interp_pts);
-  //     NS_XCTS_NOROT<eos_t> solver(bconfig);
-  //     solver.set_h_cut() = h_cut;
-  //     solver.set_eos_file() = eos_file;
-  //     solver.set_eos_type() = eos_type;
-  //     return solver;
-  //   } else if(eos_type == "Cold_PWPoly") {
-  //     using namespace Kadath::Margherita;
-  //     using eos_t = Kadath::Margherita::Cold_PWPoly;
-  //     EOS<eos_t,PRESSURE>::init(eos_file, h_cut);
-  //     NS_XCTS_NOROT<eos_t> solver(bconfig);
-  //     solver.set_h_cut() = h_cut;
-  //     solver.set_eos_file() = eos_file;
-  //     solver.set_eos_type() = eos_type;
-  //     return solver;
-  //   } 
-  //   throw std::invalid_argument("\nInvalid EOS Type\n)");
-  // }
+    // get the minimal and maximal radius from the adapted domain
+    auto [r_min, r_max] = Kadath::bco_utils::get_rmin_rmax(*space, 1);
+
+    std::cout << "Rmin/max: " << r_min << " " << r_max << std::endl;
+
+    // set new resolutions in each spatial dimension
+    Dim_array res(ndim);
+    res.set(0) = (*bconfig)(BCO_RES);
+    res.set(1) = res(0);
+    res.set(2) = res(0) - 1;
+
+    // FIXME not sure if it's only about oddness...
+    if(res(0) % 2 == 0 || res(2) % 2 != 0){
+      std::cout << "New Resolution is invalid.  Must be odd (9,11,13,etc)" << std::endl;
+      std::_Exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Resolution of new space: "
+      << res(0) << " (r), "
+      << res(1) << " (theta), "
+      << res(2) << " (phi)" << std::endl;
+
+    // get the type of the colocation points
+    int type_coloc = space->get_type_base();
+
+    // Update config
+    bconfig->set(BCO_PARAMS::RIN)  = 0.5 * r_min;
+    bconfig->set(BCO_PARAMS::ROUT) = 1.5 * r_max;
+    bconfig->set(BCO_PARAMS::RMID) = r_max;
+    // end update config
+    
+    // setup radius bounds of the domains
+
+    int ndom = 4 + (*bconfig)(BCO_PARAMS::NSHELLS);
+    std::vector<double> bounds(ndom-1);
+    Kadath::bco_utils::set_NS_bounds(bounds, *bconfig);
+    
+    Kadath::bco_utils::print_bounds("New bounds: ", bounds);
+
+    // get origin of nucleus domain
+    Point center = space->get_domain(0)->get_center();
+
+    // initialize space with new resolution and domain decomposition
+    Space_spheric_adapted new_space(type_coloc, center, res, bounds);
+    Base_tensor basis(new_space, CARTESIAN_BASIS);
+
+    // get adapted domains to update the radius
+    const Domain_shell_outer_adapted* new_outer_adapted = dynamic_cast<const Domain_shell_outer_adapted*>(new_space.get_domain(1));
+    const Domain_shell_inner_adapted* new_inner_adapted = dynamic_cast<const Domain_shell_inner_adapted*>(new_space.get_domain(2));
+
+    // update adapted domain mapping
+    Kadath::bco_utils::interp_adapted_mapping(new_outer_adapted, 1, old_space_radius);
+    Kadath::bco_utils::interp_adapted_mapping(new_inner_adapted, 1, old_space_radius);
+
+    // setup new fields
+    // initialize to one or zero first
+    Scalar new_conf(new_space);
+    new_conf = 1.;
+    new_conf.std_base();
+
+    Scalar new_lapse(new_space);
+    new_lapse = 1.;
+    new_lapse.std_base();
+
+    Vector new_shift(new_space, CON, basis);
+    for (int i = 1; i <= 3; i++)
+      new_shift.set(i).annule_hard();
+    new_shift.std_base();
+
+    Scalar new_logh(new_space);
+    new_logh.annule_hard();
+    new_logh.std_base();
+
+    // end setup new fields
+    
+    // import data from fields in the old space
+    new_conf.import(*conformal_factor);
+    new_lapse.import(*lapse);
+    new_logh.import(*logh);
+
+    new_shift.set(1).import(shift->set(1));
+    new_shift.set(2).import(shift->set(2));
+    new_shift.set(3).import(shift->set(3));
+
+    // end import old fields
+
+    // enforce spectral decomposition compatible with the parities
+    new_lapse.std_base();
+    new_conf.std_base();
+    new_logh.std_base();
+    new_shift.std_base();
+    
+    // output data  
+    bconfig->set_filename(outputfile);
+    Kadath::bco_utils::save_to_file(new_space, *bconfig, new_conf, new_lapse, new_shift, new_logh);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Ensure all ranks have the same config file
+    bconfig->set_filename(outputfile);
+    bconfig->open_config();
+    
+    // Update stored fields and containers
+    reset_all_ptrs();
+    load_solution_from_file();
+    initialize_support_containers();
+  }
 }
