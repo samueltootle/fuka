4d713c8ede731a5ec74a1959427258d6dc9e1787
s.tootle <sdtootle@gmail.com>
2024-02-23 17:07:28 -0800

    initial commit - diffrot codes.  WIP

--- include/FUKA_Solvers/NS_XCTS.hpp
+++ include/FUKA_Solvers/NS_XCTS.hpp
@@ -161,44 +161,30 @@ struct NS_XCTS_UNIFORM_ROT : NS_XCTS_BASE {
       int const rank_ = 0);
 };
 
-struct NS_XCTS_DIFFROT {
-  using base_space_t = Space_spheric_adapted;
-  using base_config_t = Kadath::FUKA_Config::kadath_config_boost<Kadath::FUKA_Config::BCO_NS_INFO>;
+template<class eos_t>
+struct NS_XCTS_DIFF_ROT : NS_XCTS_BASE {
 
-  using cfgen_t  = CoordFields<base_space_t>;
-  using cfary_t  = std::array<std::optional<Vector>, NUM_VECTORS>;
-  
-  ptr_data_member(base_space_t, space, unique);
-  ptr_data_member(base_config_t, bconfig, unique);
-
-  protected:
-  static constexpr int ndom{3};
-  int verbosity;
-  ::Kadath::FUKA_Config::STAGES solver_stage;
-
-  // Support containers
-  ptr_data_member(Base_tensor, basis, unique);
-  ptr_data_member(Metric_flat, fmet, unique);
-  ptr_data_member(cfgen_t, cfields, unique);
-  ptr_data_member(cfary_t, coord_vectors, unique);
-  ptr_data_member(ns_sequence, seq, unique);
-
-  // Variable fields - i.e. Solution
-  ptr_data_member(Scalar, conformal_factor, unique);
-  ptr_data_member(Scalar, lapse, unique);
-  ptr_data_member(Vector, shift, unique);
-  ptr_data_member(Scalar, logh, unique);
-  ptr_data_member(Scalar, diff_omega, unique);
+  private:
+  void syst_init();
+  void print_diagnostics(const int ite, const double conv) const override;
+  void update_config_quantities() override;
+  void initialize_spinup();
+  internal_variable(double, axis_ratio);
+  ptr_data_member(Parameter_sequence<DIFFROT_PARAMS>, spinup, unique);
 
   public:
-  void save_to_file() const;
+  void setup_syst();
+  bool increment_spin();
+  std::string converged_filename(const std::string stage) const;
 
-  NS_XCTS_DIFFROT() = default;
-  NS_XCTS_DIFFROT(std::string filename);
-  NS_XCTS_DIFFROT(base_config_t& config_);
+  NS_XCTS_DIFF_ROT() = default;
+  NS_XCTS_DIFF_ROT(base_config_t* config_, ns_sequence const & seq_, 
+    Parameter_sequence<BCO_PARAMS> const & res_, std::string outputdir_, 
+      int const rank_ = 0);
 };
 /** @}*/
 };
 #include "NS_XCTS_BASE.cpp"
 #include "NS_XCTS_NOROT.cpp"
 #include "NS_XCTS_UNIFORM_ROT.cpp"
+#include "NS_XCTS_DIFF_ROT.cpp"
