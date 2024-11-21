26f31dc7dd54e9bd0277129076b198129b006db1
s.tootle <sdtootle@gmail.com>
2024-11-21 12:08:13 -0800

    clang formatted

--- codes/FUKAv2_Solvers/NS_DIFFROT/src/setup_from_iso.cpp
+++ codes/FUKAv2_Solvers/NS_DIFFROT/src/setup_from_iso.cpp
@@ -1,13 +1,15 @@
 #include "Configurator/config_bco.hpp"
+#include "EOS/EOS.hh"
 #include "Solvers/ns_isotropic/ns_isotropic_exporter.hpp"
 #include "bco_utilities.hpp"
-#include "EOS/EOS.hh"
 // #include "EOS/standalone/tov.hh"
 
 // Kadath includes
 #include "kadath.hpp"
-#include "kadath_adapted_polar.hpp"
 #include "kadath_adapted.hpp"
+#include "kadath_adapted_polar.hpp"
+
+#include <mpi.h>
 
 using namespace Kadath;
 using namespace Kadath::FUKA_Config;
@@ -15,163 +17,173 @@ using namespace Kadath::FUKA_Config;
 using config_t = kadath_config_boost<BCO_NS_INFO>;
 using input_reader_t = Kadath::FUKA_Solvers::CFMS_NS_ISO_Exporter;
 
-template<class eos_t>
+template <class eos_t>
 void ISO_XCTS_convert(config_t& in_bconfig) {
 
-    // Convert from QI solution
-    input_reader_t input_reader(in_bconfig.config_filename_abs());
-    
-    auto& old_space = input_reader.get_space();
-    Scalar old_space_radius(*old_space);
-    old_space_radius = 0.;
-    // get the radius from each domain
-    for(int i = 0; i < old_space->get_nbr_domains(); ++i)
-        old_space_radius.set_domain(i) = old_space->get_domain(i)->get_radius();
-    // get the adapted radius of the adapted domain
-    const Domain_polar_shell_outer_adapted* old_outer_adapted = 
-        dynamic_cast<const Domain_polar_shell_outer_adapted*> (old_space->get_domain(old_space->ADAPTED_OUTER)) ;
-    old_space_radius.set_domain(old_space->ADAPTED_OUTER) = old_outer_adapted->get_outer_radius();
+  // Convert from QI solution
+  input_reader_t input_reader(in_bconfig.config_filename_abs());
 
-    // define a standard decomposition, compatible with the parity of this field
-    old_space_radius.std_base();
-    //end setup old radius field
+  auto& old_space = input_reader.get_space();
+  Scalar old_space_radius(*old_space);
+  old_space_radius = 0.;
+  // get the radius from each domain
+  for (int i = 0; i < old_space->get_nbr_domains(); ++i)
+    old_space_radius.set_domain(i) = old_space->get_domain(i)->get_radius();
+  // get the adapted radius of the adapted domain
+  const Domain_polar_shell_outer_adapted* old_outer_adapted =
+      dynamic_cast<const Domain_polar_shell_outer_adapted*>(
+          old_space->get_domain(old_space->ADAPTED_OUTER));
+  old_space_radius.set_domain(old_space->ADAPTED_OUTER) =
+      old_outer_adapted->get_outer_radius();
 
-    // get the minimal and maximal radius from the adapted domain
-    auto [r_min, r_max] = Kadath::bco_utils::get_rmin_rmax(*old_space, 1);
+  // define a standard decomposition, compatible with the parity of this field
+  old_space_radius.std_base();
+  //end setup old radius field
 
-    config_t bconfig = in_bconfig;
-    bconfig.set(BCO_PARAMS::DIM) = 3;
-    bconfig.set_filename("initns_xcts.info");
-    
-    // number of dimensions
-    const int dim = bconfig(DIM);
+  // get the minimal and maximal radius from the adapted domain
+  auto [r_min, r_max] = Kadath::bco_utils::get_rmin_rmax(*old_space, 1);
 
-    // collocation point type and number of collocation points per domain
-    int type_coloc = CHEB_TYPE;
-    Dim_array res(dim);
-    res.set(0) = bconfig(BCO_RES);
-    res.set(1) = bconfig(BCO_RES);
-    res.set(2) = bconfig(BCO_RES)-1;
+  config_t bconfig = in_bconfig;
+  bconfig.set(BCO_PARAMS::DIM) = 3;
+  bconfig.set_filename("initns_xcts.info");
 
-    // physical center of the system, arbitrary
-    Point center(dim);
-    for (int i = 1; i <= dim; i++)
-        center.set(i) = 0;
+  // number of dimensions
+  const int dim = bconfig(DIM);
 
-    // domain boundaries, i.e. radii for each domain of the single star space
-    int ndom      = 4;
-    Array<double> bounds(ndom - 1);
-    bounds.set(0) = bconfig(RIN);
-    bounds.set(1) = bconfig(RMID);
-    bounds.set(2) = bconfig(ROUT);
+  // collocation point type and number of collocation points per domain
+  int type_coloc = CHEB_TYPE;
+  Dim_array res(dim);
+  res.set(0) = bconfig(BCO_RES);
+  res.set(1) = bconfig(BCO_RES);
+  res.set(2) = bconfig(BCO_RES) - 1;
 
-    // generate a full single star space including compactification to infinity
-    Space_spheric_adapted space(type_coloc, center, res, bounds);
+  // physical center of the system, arbitrary
+  Point center(dim);
+  for (int i = 1; i <= dim; i++)
+    center.set(i) = 0;
 
-    // use a basis of cartesian type
-    Base_tensor basis(space, CARTESIAN_BASIS);
+  // domain boundaries, i.e. radii for each domain of the single star space
+  int ndom = 4;
+  Array<double> bounds(ndom - 1);
+  bounds.set(0) = bconfig(RIN);
+  bounds.set(1) = bconfig(RMID);
+  bounds.set(2) = bconfig(ROUT);
 
-      // get adapted domains to update the radius
-    const Domain_shell_outer_adapted* new_outer_adapted = dynamic_cast<const Domain_shell_outer_adapted*>(space.get_domain(1));
-    const Domain_shell_inner_adapted* new_inner_adapted = dynamic_cast<const Domain_shell_inner_adapted*>(space.get_domain(2));
+  // generate a full single star space including compactification to infinity
+  Space_spheric_adapted space(type_coloc, center, res, bounds);
 
-    // update adapted domain mapping
-    Kadath::bco_utils::interp_adapted_mapping(new_outer_adapted, 1, old_space_radius);
-    Kadath::bco_utils::interp_adapted_mapping(new_inner_adapted, 1, old_space_radius);
+  // use a basis of cartesian type
+  Base_tensor basis(space, CARTESIAN_BASIS);
 
-    Scalar logh(space);
-    logh.annule_hard();
+  // get adapted domains to update the radius
+  const Domain_shell_outer_adapted* new_outer_adapted =
+      dynamic_cast<const Domain_shell_outer_adapted*>(space.get_domain(1));
+  const Domain_shell_inner_adapted* new_inner_adapted =
+      dynamic_cast<const Domain_shell_inner_adapted*>(space.get_domain(2));
 
-    Scalar omega(logh);
+  // update adapted domain mapping
+  Kadath::bco_utils::interp_adapted_mapping(new_outer_adapted, 1,
+                                            old_space_radius);
+  Kadath::bco_utils::interp_adapted_mapping(new_inner_adapted, 1,
+                                            old_space_radius);
 
-    // conformal factor is initialized to one, representing flat space as an initial guess
-    Scalar conf(space);
-    conf = 1.;
+  Scalar logh(space);
+  logh.annule_hard();
 
-    // same for the lapse
-    Scalar lapse(conf);
+  Scalar omega(logh);
 
-    Vector shift(space, CON, basis);
-    shift.annule_hard();
-    
-    auto convert_fields= [&](const size_t dom) {
-        auto npts = space.get_domain(dom)->get_nbr_points();
-        Index pos(npts);
-        Val_domain xx = space.get_domain(dom)->get_cart(1);
-        Val_domain yy = space.get_domain(dom)->get_cart(2);
-        Val_domain zz = space.get_domain(dom)->get_cart(3);
-        do {
-            if(!(dom == ndom-1 && pos(0) == npts(0) - 1)) {
-            auto x = xx(pos);
-            auto y = yy(pos);
-            auto z = zz(pos);
-            auto all_data = input_reader.export_pointwise(x, y, z);
-            auto const rho = all_data[input_reader_t::OUTPUT_VARS::RHO];
-            auto const h = EOS<eos_t,DENSITY>::h_cold__rho(rho);
-            logh.set_domain(dom).set(pos) = (std::log(h) < 0) ? 0. : std::log(h);
-            lapse.set_domain(dom).set(pos) = all_data[input_reader_t::OUTPUT_VARS::ALPHA];
-            
-            // approximation
-            auto const psi4 = all_data[input_reader_t::OUTPUT_VARS::G11];
-            auto const psi2 = std::sqrt(psi4);
-            auto const psi  = std::sqrt(psi2);
-            conf.set_domain(dom).set(pos) = psi;
+  // conformal factor is initialized to one, representing flat space as an initial guess
+  Scalar conf(space);
+  conf = 1.;
 
-            auto iso_omega(input_reader.get_omega());
-            double r2_xy = x * x + y * y;
-            double r_xy = std::sqrt(r2_xy);
-            Point abs_coords(2);
-            abs_coords.set(1) = r_xy;
-            abs_coords.set(2) = z;
-            omega.set_domain(dom).set(pos) = iso_omega->val_point(abs_coords);
+  // same for the lapse
+  Scalar lapse(conf);
 
-            shift.set(1).set_domain(dom).set(pos) = all_data[input_reader_t::OUTPUT_VARS::BETA1];
-            shift.set(2).set_domain(dom).set(pos) = all_data[input_reader_t::OUTPUT_VARS::BETA2];
-            shift.set(3).set_domain(dom).set(pos) = all_data[input_reader_t::OUTPUT_VARS::BETA3];
-            } else {
-              lapse.set_domain(dom).set(pos)= 1.;
-              conf.set_domain(dom).set(pos) = 1.;
-              shift.set(1).set_domain(dom).set(pos) = 0;
-              shift.set(2).set_domain(dom).set(pos) = 0;
-              shift.set(3).set_domain(dom).set(pos) = 0;
-              logh.set_domain(dom).set(pos) = 0.;
-            }
-        }while(pos.inc());
-    };
-    for(int i = 0; i < ndom; ++i)
-        convert_fields(i);
+  Vector shift(space, CON, basis);
+  shift.annule_hard();
 
-    for (int d = 2; d < ndom; ++d)
-        logh.set_domain(d).annule_hard();
+  auto convert_fields = [&](const size_t dom) {
+    auto npts = space.get_domain(dom)->get_nbr_points();
+    Index pos(npts);
+    Val_domain xx = space.get_domain(dom)->get_cart(1);
+    Val_domain yy = space.get_domain(dom)->get_cart(2);
+    Val_domain zz = space.get_domain(dom)->get_cart(3);
+    do {
+      if (!(dom == ndom - 1 && pos(0) == npts(0) - 1)) {
+        auto x = xx(pos);
+        auto y = yy(pos);
+        auto z = zz(pos);
+        auto all_data = input_reader.export_pointwise(x, y, z);
+        auto const rho = all_data[input_reader_t::OUTPUT_VARS::RHO];
+        auto const h = EOS<eos_t, DENSITY>::h_cold__rho(rho);
+        logh.set_domain(dom).set(pos) = (std::log(h) < 0) ? 0. : std::log(h);
+        lapse.set_domain(dom).set(pos) =
+            all_data[input_reader_t::OUTPUT_VARS::ALPHA];
 
-    // shift is zero in the beginning and in general for TOV solutions
-    
-    shift.std_base();
-    logh.std_base();
-    conf.std_base();
-    lapse.std_base();
-    omega.std_base();
+        // approximation
+        auto const psi4 = all_data[input_reader_t::OUTPUT_VARS::G11];
+        auto const psi2 = std::sqrt(psi4);
+        auto const psi = std::sqrt(psi2);
+        conf.set_domain(dom).set(pos) = psi;
 
-    for(int i = 1; i <=3; ++i)
-        shift.set(i).coef();
-    logh.coef();
-    conf.coef();
-    lapse.coef();
-    omega.coef();
-    // finished create the fields
-    for(int i = 0; i < BCO_FIELDS::NUM_BCO_FIELDS; ++i)
-        bconfig.set_field(i) = false;
-    bconfig.set_field(BCO_FIELDS::LAPSE) = true;
-    bconfig.set_field(BCO_FIELDS::CONF)  = true;
-    bconfig.set_field(BCO_FIELDS::SHIFT) = true;
-    bconfig.set_field(BCO_FIELDS::LOGH)  = true;
-    bconfig.set_field(BCO_FIELDS::DIFF_OMEGA) = true;
-    // write space and config to files on one processor
-    bconfig.set_stage(PRE) = false;
-    bco_utils::save_to_file(space, bconfig, conf, lapse, shift, logh, omega);
+        auto iso_omega(input_reader.get_omega());
+        double r2_xy = x * x + y * y;
+        double r_xy = std::sqrt(r2_xy);
+        Point abs_coords(2);
+        abs_coords.set(1) = r_xy;
+        abs_coords.set(2) = z;
+        omega.set_domain(dom).set(pos) = iso_omega->val_point(abs_coords);
+
+        shift.set(1).set_domain(dom).set(pos) =
+            all_data[input_reader_t::OUTPUT_VARS::BETA1];
+        shift.set(2).set_domain(dom).set(pos) =
+            all_data[input_reader_t::OUTPUT_VARS::BETA2];
+        shift.set(3).set_domain(dom).set(pos) =
+            all_data[input_reader_t::OUTPUT_VARS::BETA3];
+      } else {
+        lapse.set_domain(dom).set(pos) = 1.;
+        conf.set_domain(dom).set(pos) = 1.;
+        shift.set(1).set_domain(dom).set(pos) = 0;
+        shift.set(2).set_domain(dom).set(pos) = 0;
+        shift.set(3).set_domain(dom).set(pos) = 0;
+        logh.set_domain(dom).set(pos) = 0.;
+      }
+    } while (pos.inc());
+  };
+  for (int i = 0; i < ndom; ++i)
+    convert_fields(i);
+
+  for (int d = 2; d < ndom; ++d)
+    logh.set_domain(d).annule_hard();
+
+  // shift is zero in the beginning and in general for TOV solutions
+
+  shift.std_base();
+  logh.std_base();
+  conf.std_base();
+  lapse.std_base();
+  omega.std_base();
+
+  for (int i = 1; i <= 3; ++i)
+    shift.set(i).coef();
+  logh.coef();
+  conf.coef();
+  lapse.coef();
+  omega.coef();
+  // finished create the fields
+  for (int i = 0; i < BCO_FIELDS::NUM_BCO_FIELDS; ++i)
+    bconfig.set_field(i) = false;
+  bconfig.set_field(BCO_FIELDS::LAPSE) = true;
+  bconfig.set_field(BCO_FIELDS::CONF) = true;
+  bconfig.set_field(BCO_FIELDS::SHIFT) = true;
+  bconfig.set_field(BCO_FIELDS::LOGH) = true;
+  bconfig.set_field(BCO_FIELDS::DIFF_OMEGA) = true;
+  // write space and config to files on one processor
+  bconfig.set_stage(PRE) = false;
+  bco_utils::save_to_file(space, bconfig, conf, lapse, shift, logh, omega);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   // initialize MPI
   int rc = MPI_Init(&argc, &argv);
 
@@ -181,7 +193,7 @@ int main(int argc, char **argv) {
   }
 
   // expecting a configuration file on execution
-  if(argc < 2) {
+  if (argc < 2) {
     std::cerr << "Usage: ./reader /<path>/<ID base name>.info" << std::endl;
     std::cerr << "e.g. ./reader converged.NS.9.info" << endl;
     std::_Exit(EXIT_FAILURE);
@@ -196,20 +208,21 @@ int main(int argc, char **argv) {
   const std::string eos_file = bconfig.eos<std::string>(EOSFILE);
   const std::string eos_type = bconfig.eos<std::string>(EOSTYPE);
 
-  if(eos_type == "Cold_PWPoly") {
+  if (eos_type == "Cold_PWPoly") {
     using eos_t = Kadath::Margherita::Cold_PWPoly;
 
-    EOS<eos_t,PRESSURE>::init(eos_file, h_cut);
+    EOS<eos_t, PRESSURE>::init(eos_file, h_cut);
 
     // call reader to output diagnostics
     ISO_XCTS_convert<eos_t>(bconfig);
-  } else if(eos_type == "Cold_Table") {
+  } else if (eos_type == "Cold_Table") {
     using eos_t = Kadath::Margherita::Cold_Table;
 
-    const int interp_pts = (bconfig.eos<int>(INTERP_PTS) == 0) ? \
-                            2000 : bconfig.eos<int>(INTERP_PTS);
+    const int interp_pts = (bconfig.eos<int>(INTERP_PTS) == 0)
+                               ? 2000
+                               : bconfig.eos<int>(INTERP_PTS);
 
-    EOS<eos_t,PRESSURE>::init(eos_file, h_cut, interp_pts);
+    EOS<eos_t, PRESSURE>::init(eos_file, h_cut, interp_pts);
 
     // call reader to output diagnostics
     ISO_XCTS_convert<eos_t>(bconfig);
--- codes/FUKAv2_Solvers/NS_DIFFROT/src/solve.cpp
+++ codes/FUKAv2_Solvers/NS_DIFFROT/src/solve.cpp
@@ -23,26 +23,26 @@
 // #include "FUKA_Solvers/NS_XCTS.hpp"
 // #include "Solvers/ns_3d_xcts/ns_3d_xcts_driver.hpp"
 #include "FUKA_Solvers/NS_XCTS_driver.cpp"
-#include "Solvers/solver_startup.hpp"
 #include "Solvers/sequences/ns_sequence.hpp"
 #include "Solvers/sequences/parameter_sequence.hpp"
 #include "Solvers/sequences/sequence_utilities.hpp"
+#include "Solvers/solver_startup.hpp"
 
 using namespace Kadath::FUKA_Config;
 using namespace Kadath::FUKA_Solvers;
 
 int main(int argc, char** argv) {
-  int rc = MPI_Init(&argc, &argv) ;
-  if (rc!=MPI_SUCCESS) {
-    cerr << "Error starting MPI" << endl ;
-    MPI_Abort(MPI_COMM_WORLD, rc) ;
+  int rc = MPI_Init(&argc, &argv);
+  if (rc != MPI_SUCCESS) {
+    cerr << "Error starting MPI" << endl;
+    MPI_Abort(MPI_COMM_WORLD, rc);
   }
-  int rank = 0 ;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank) ;
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   using config_t = kadath_config_boost<BCO_NS_INFO>;
   using InitSolver = Initialize_Solver<config_t>;
-  
+
   // Initialize static member variables
   InitSolver::input_configname = "initial_ns.info";
   InitSolver::bconfig;
@@ -52,10 +52,10 @@ int main(int argc, char** argv) {
   InitSolver::init_solver(argc, argv);
   config_t bconfig = InitSolver::bconfig;
 
-  if(InitSolver::example_setup) {
-    if(rank == 0) {
-      // Generate <example name>.info and terminate      
-      if(InitSolver::minimal_config) {
+  if (InitSolver::example_setup) {
+    if (rank == 0) {
+      // Generate <example name>.info and terminate
+      if (InitSolver::minimal_config) {
         bconfig.set_minimal_defaults();
         bconfig.set_stage(STAGES::TOTAL_BC) = false;
         bconfig.set_stage(STAGES::UNIFORM_ROT) = true;
@@ -73,28 +73,30 @@ int main(int argc, char** argv) {
     // that contains sequences _init/_final
     bconfig.open_config();
     bconfig.control(CONTROLS::SEQUENCES) = InitSolver::setup_first;
-    if(bconfig.set_stage(STAGES::TOTAL_BC)) {
-      bconfig.set_stage(STAGES::TOTAL_BC) = false;  
+    if (bconfig.set_stage(STAGES::TOTAL_BC)) {
+      bconfig.set_stage(STAGES::TOTAL_BC) = false;
       bconfig.set_stage(STAGES::UNIFORM_ROT) = true;
     }
 
     Tree tree;
     pt::read_info(bconfig.config_filename_abs(), tree);
     auto N = number_of_sequences(tree, MBCO_PARAMS, "ns");
-    if(N > 1) {
-      if(rank == 0) {
+    if (N > 1) {
+      if (rank == 0) {
         std::cerr << "Only sequences along one component is allowed.\n";
         std::_Exit(EXIT_FAILURE);
       }
     }
 
-    Parameter_sequence<BCO_PARAMS> resolution = parse_seq_tree(tree, "ns", "res", BCO_PARAMS::BCO_RES);
+    Parameter_sequence<BCO_PARAMS> resolution =
+        parse_seq_tree(tree, "ns", "res", BCO_PARAMS::BCO_RES);
     verify_resolution_sequence(bconfig, resolution);
 
     ns_sequence seq = find_ns_sequence(tree);
-    if(seq.is_set()) {
-      auto [ branch_name, key, val ] = find_leaf(tree, "N");
-      if(!key.empty()) seq.set_N(std::stoi(val));
+    if (seq.is_set()) {
+      auto [branch_name, key, val] = find_leaf(tree, "N");
+      if (!key.empty())
+        seq.set_N(std::stoi(val));
     }
 
     verify_ns_fixing_values(bconfig, seq);
