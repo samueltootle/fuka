f95c93f41f3c8a5a8837411f1545bdcece5a2819
Samuel Tootle <sdtootle@gmail.com>
2024-12-19 21:05:59 -0800

    Refactor to use FUKA_EOS utils clang formatted

--- codes/FUKAv2_Solvers/NS_isotropic/src/solve.cpp
+++ codes/FUKAv2_Solvers/NS_isotropic/src/solve.cpp
@@ -3,7 +3,7 @@
  * This file is part of the KADATH library and published under
  * https://arxiv.org/abs/2103.09911
  *
- * Author: 
+ * Author:
  * Samuel D. Tootle <tootle@itp.uni-frankfurt.de>
  * L. Jens. Papenfort
  *
@@ -20,28 +20,30 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include "mpi.h"
+#include "EOS/FUKA_EOS_Utilities.hh"
 #include "Solvers/ns_isotropic/ns_isotropic_driver.hpp"
-#include "Solvers/solver_startup.hpp"
 #include "Solvers/sequences/ns_sequence.hpp"
 #include "Solvers/sequences/parameter_sequence.hpp"
 #include "Solvers/sequences/sequence_utilities.hpp"
+#include "Solvers/solver_startup.hpp"
+#include "mpi.h"
 
 using namespace Kadath::FUKA_Config;
 using namespace Kadath::FUKA_Solvers;
+using namespace Kadath::FUKA_EOS;
 
 int main(int argc, char** argv) {
-  int rc = MPI_Init(&argc, &argv) ;
-  if (rc!=MPI_SUCCESS) {
-    cerr << "Error starting MPI" << endl ;
-    MPI_Abort(MPI_COMM_WORLD, rc) ;
+  int rc = MPI_Init(&argc, &argv);
+  if (rc != MPI_SUCCESS) {
+    cerr << "Error starting MPI" << endl;
+    MPI_Abort(MPI_COMM_WORLD, rc);
   }
-  int rank = 0 ;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank) ;
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   using config_t = kadath_config_boost<BCO_ISO_NS_INFO>;
   using InitSolver = Initialize_Solver<config_t>;
-  
+
   // Initialize static member variables
   InitSolver::input_configname = "initial_2dns.info";
   InitSolver::rank = rank;
@@ -50,10 +52,10 @@ int main(int argc, char** argv) {
   InitSolver::init_solver(argc, argv);
   config_t bconfig = InitSolver::bconfig;
 
-  if(InitSolver::example_setup) {
-    if(rank == 0) {
-      // Generate <example name>.info and terminate      
-      if(InitSolver::minimal_config) {
+  if (InitSolver::example_setup) {
+    if (rank == 0) {
+      // Generate <example name>.info and terminate
+      if (InitSolver::minimal_config) {
         bconfig.set_minimal_defaults();
         bconfig.write_minimal_config();
       } else {
@@ -67,11 +69,12 @@ int main(int argc, char** argv) {
     // that contains sequences _init/_final
     bconfig.open_config();
     bconfig.control(CONTROLS::SEQUENCES) = InitSolver::setup_first;
+    EOS_initialize::init(bconfig);
 
     auto tree = bconfig.get_config_tree();
     auto N = number_of_sequences(tree, MBCO_PARAMS, "ns");
-    if(N > 1) {
-      if(rank == 0) {
+    if (N > 1) {
+      if (rank == 0) {
         std::cerr << "Only sequences along one component is allowed.\n";
         std::_Exit(EXIT_FAILURE);
       }
@@ -83,13 +86,13 @@ int main(int argc, char** argv) {
     ns_sequence seq = find_ns_sequence(tree);
     verify_ns_fixing_values(bconfig, seq);
 
-    if(!seq.is_set() && !bconfig.control(CONTROLS::SEQUENCES)) {
+    if (!seq.is_set() && !bconfig.control(CONTROLS::SEQUENCES)) {
       initialize_config_from_fixing_values(bconfig, seq);
       ns_isotropic_driver(bconfig, resolution, InitSolver::outputdir, &seq);
     } else {
-      
-      auto [ branch_name, key, val ] = find_leaf(tree, "N");
-      if(!key.empty()) seq.set_N(std::stoi(val));
+      auto [branch_name, key, val] = find_leaf(tree, "N");
+      if (!key.empty())
+        seq.set_N(std::stoi(val));
 
       ns_isotropic_sequence(bconfig, seq, resolution, InitSolver::outputdir);
     }
