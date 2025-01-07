f0988abc45bc03d4961d9ac5473705d682a9fe07
S.Tootle <sdtootle@gmail.com>
2025-01-07 16:01:10 -0700

    more grhayl testing

--- include/EOS/ghl_eos_helpers/ghl_hybrid_helpers.hpp
+++ include/EOS/ghl_eos_helpers/ghl_hybrid_helpers.hpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <string>
 #include "EOS/standalone/EOS_parfile_parser.hpp"
+#include "EOS/standalone/brent.hh"
 
 namespace Kadath {
 namespace GHL_EOS {
@@ -37,6 +38,52 @@ inline auto ghl_setup_polytrope(std::string polytrope_file) {
   return eos_params;
 }
 
+
+/**
+ * Update GRHaYL beta equilibrium table to be more useful for elliptic solvers.
+ */
+inline void ghl_modify_beta_equ_table(std::unique_ptr<ghl_eos_parameters>& eos) {
+  // Find the bounded slice of interest
+  std::vector<double> lp_of_lr_v;
+  std::vector<double> le_of_lr_v;
+  std::vector<double> lh_of_lr_v;
+  std::vector<double> logrho_v;
+  for(int ir = 0; ir < eos->N_rho; ir++) {
+    const double rho = exp(eos->table_logrho[ir]);
+    const double h = exp(eos->lh_of_lr[ir]);
+    // if(rho < eos->rho_min || rho > eos->rho_max || h < 1) {
+    //   continue;
+    // }
+    lp_of_lr_v.push_back(eos->lp_of_lr[ir]);
+    le_of_lr_v.push_back(eos->le_of_lr[ir]);
+    lh_of_lr_v.push_back(eos->lh_of_lr[ir]);
+    logrho_v.push_back(eos->table_logrho[ir]);
+  }
+
+  // Update 1D table slice size
+  eos->N_rho = lp_of_lr_v.size();
+
+  // Free old arrays
+  free(eos->lp_of_lr);
+  free(eos->le_of_lr);
+  free(eos->lh_of_lr);
+  free(eos->table_logrho);
+
+  // Allocate new arrays
+  eos->lp_of_lr = (double *)malloc(sizeof(double) * eos->N_rho);
+  eos->le_of_lr = (double *)malloc(sizeof(double) * eos->N_rho);
+  eos->lh_of_lr = (double *)malloc(sizeof(double) * eos->N_rho);
+  eos->table_logrho = (double *)malloc(sizeof(double) * eos->N_rho);
+
+  // Copy data
+  for(int ir = 0; ir < eos->N_rho; ir++) {
+    eos->lp_of_lr[ir] = lp_of_lr_v[ir];
+    eos->le_of_lr[ir] = le_of_lr_v[ir];
+    eos->lh_of_lr[ir] = lh_of_lr_v[ir];
+    eos->table_logrho[ir] = logrho_v[ir];
+  }
+}
+
 inline auto populate_ghl_tabulated(std::string table_par_file) {
   using namespace Kadath::FUKA_EOS;
   std::unique_ptr<ghl_eos_parameters> eos_params;
@@ -50,6 +97,22 @@ inline auto populate_ghl_tabulated(std::string table_par_file) {
       parser.T_min, parser.T_max, &eos);
   ghl_tabulated_compute_Ye_P_eps_of_rho_beq_constant_T(1.1e-2, &eos);
   eos_params = std::make_unique<ghl_eos_parameters>(eos);
+
+  ghl_modify_beta_equ_table(eos_params);
+
+  // for(int ir = 0; ir < eos_params->N_rho; ir++) {
+  //   std::cout << std::exp(eos_params->table_logrho[ir]) << " "
+  //             << std::exp(eos_params->lh_of_lr[ir]) << " "
+  //             << std::exp(eos_params->lp_of_lr[ir]) << " "
+  //             << std::exp(eos_params->le_of_lr[ir]) << std::endl;
+  // }
+  // std::cout << "##################################################################################\n";
+  // for(int ir = 0; ir < eos_params->N_rho; ir++) {
+  //   std::cout << eos_params->table_logrho[ir] << " "
+  //             << eos_params->lh_of_lr[ir] << " "
+  //             << eos_params->lp_of_lr[ir] << " "
+  //             << eos_params->le_of_lr[ir] << std::endl;
+  // }
   return eos_params;
 }
 
