e3db19c8fd5f938f8c7f9271ddc67855a716639a
S.Tootle <sdtootle@gmail.com>
2025-01-28 16:24:16 -0700

    Clang-formatted. Add new EOS init

--- codes/FUKAv2_Solvers/BHNS/src/solve.cpp
+++ codes/FUKAv2_Solvers/BHNS/src/solve.cpp
@@ -3,7 +3,7 @@
  * This file is part of the KADATH library and published under
  * https://arxiv.org/abs/2103.09911
  *
- * Author: 
+ * Author:
  * Samuel D. Tootle <tootle@itp.uni-frankfurt.de>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -19,26 +19,25 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include "mpi.h"
-#include "Solvers/solver_startup.hpp"
-#include "Solvers/bhns_xcts/bhns_xcts_setup.hpp"
 #include "Solvers/bhns_xcts/bhns_xcts_driver.hpp"
+#include "Solvers/bhns_xcts/bhns_xcts_setup.hpp"
 #include "Solvers/sequences/parameter_sequence.hpp"
+#include "Solvers/solver_startup.hpp"
+#include "mpi.h"
 
 int main(int argc, char** argv) {
-  int rc = MPI_Init(&argc, &argv) ;
-  if (rc!=MPI_SUCCESS) {
-    cerr << "Error starting MPI" << endl ;
-    MPI_Abort(MPI_COMM_WORLD, rc) ;
+  int rc = MPI_Init(&argc, &argv);
+  if (rc != MPI_SUCCESS) {
+    cerr << "Error starting MPI" << endl;
+    MPI_Abort(MPI_COMM_WORLD, rc);
   }
-  int rank, err = 0 ;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank) ;
+  int rank, err = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   namespace solvers = ::Kadath::FUKA_Solvers;
-  using config_t = kadath_config_boost<BIN_INFO>;  
+  using config_t = kadath_config_boost<BIN_INFO>;
   using InitSolver = solvers::Initialize_Solver<config_t>;
-  
-  
+
   // Initialize static member variables
   InitSolver::input_configname = "initial_bhns.info";
   InitSolver::bconfig;
@@ -47,22 +46,23 @@ int main(int argc, char** argv) {
   // Run initialize routine based on CLI arguments
   InitSolver::init_solver(argc, argv);
   config_t bconfig = InitSolver::bconfig;
-  
-  if(InitSolver::example_setup) {
-    if(rank == 0) {
-      bconfig.initialize_binary({"ns","bh"});
-      if(InitSolver::minimal_config) {
+
+  if (InitSolver::example_setup) {
+    if (rank == 0) {
+      bconfig.initialize_binary({"ns", "bh"});
+      if (InitSolver::minimal_config) {
         bconfig.set_minimal_defaults();
       } else {
         bconfig.set_defaults();
       }
 
-      bconfig.set(BCO_PARAMS::MCH, NODES::BCO2) = bconfig(BCO_PARAMS::MADM, NODES::BCO1);
+      bconfig.set(BCO_PARAMS::MCH, NODES::BCO2) =
+          bconfig(BCO_PARAMS::MADM, NODES::BCO1);
       bconfig.set(BIN_PARAMS::DIST) = 35;
       bconfig.control(CONTROLS::SEQUENCES) = InitSolver::setup_first;
 
-      if(InitSolver::minimal_config) {
-        bconfig.write_minimal_config();        
+      if (InitSolver::minimal_config) {
+        bconfig.write_minimal_config();
       } else {
         solvers::bhns_xcts_setup_bin_config(bconfig);
         bconfig.write_config();
@@ -71,36 +71,42 @@ int main(int argc, char** argv) {
   } else {
     bconfig.open_config();
     bconfig.control(CONTROLS::SEQUENCES) = InitSolver::setup_first;
+    EOS_initialize::init(bconfig, NODES::BCO1);
 
     auto tree = bconfig.get_config_tree();
     auto N = solvers::number_of_sequences_binary(tree);
-    if(N > 1) {
-      if(rank == 0) {
+    if (N > 1) {
+      if (rank == 0) {
         std::cerr << "Only sequences along one component is allowed.\n";
         std::_Exit(EXIT_FAILURE);
       }
     }
 
-    auto resolution = solvers::parse_seq_tree(tree, "binary", "res", BIN_PARAMS::BIN_RES);
+    auto resolution =
+        solvers::parse_seq_tree(tree, "binary", "res", BIN_PARAMS::BIN_RES);
     solvers::verify_resolution_sequence(bconfig, resolution);
-    
+
     auto seq = solvers::find_sequence_binary(tree);
     auto seq_bin = solvers::find_sequence(tree, MBIN_PARAMS, "binary");
     int err = EXIT_SUCCESS;
-    
-    if(!(seq.is_set() || seq_bin.is_set()) && !bconfig.control(CONTROLS::SEQUENCES))
+
+    if (!(seq.is_set() || seq_bin.is_set()) &&
+        !bconfig.control(CONTROLS::SEQUENCES))
       err = bhns_xcts_driver(bconfig, resolution, InitSolver::outputdir);
     else {
-      
-      auto [ branch_name, key, val ] = find_leaf(tree, "N");
-      if(!key.empty()) seq.set_N(std::stoi(val));
-      if(seq.is_set())
-        err = bhns_xcts_sequence(bconfig, seq, resolution, InitSolver::outputdir);
+
+      auto [branch_name, key, val] = find_leaf(tree, "N");
+      if (!key.empty())
+        seq.set_N(std::stoi(val));
+      if (seq.is_set())
+        err =
+            bhns_xcts_sequence(bconfig, seq, resolution, InitSolver::outputdir);
       else
-        err = bhns_xcts_sequence(bconfig, seq_bin, resolution, InitSolver::outputdir);
+        err = bhns_xcts_sequence(bconfig, seq_bin, resolution,
+                                 InitSolver::outputdir);
     }
-  } 
-  
+  }
+
   MPI_Finalize();
   return err;
 }
