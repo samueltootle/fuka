4dc2b145e171b1bcc12c9901a51450d749f6aeb5
S.Tootle <sdtootle@gmail.com>
2025-05-20 16:28:41 -0600

    fix outdated logic

--- include/Solvers/ns_3d_xcts/ns_3d_xcts_solver_imp.cpp
+++ include/Solvers/ns_3d_xcts/ns_3d_xcts_solver_imp.cpp
@@ -87,9 +87,7 @@ int ns_3d_xcts_solver<eos_t, config_t, space_t>::solve() {
   if (rank == 0)
     std::cout << "Last stage: " << last_stage << "\n";
 
-  double const& final_chi = (!bconfig.control(CONTROLS::ITERATIVE_CHI))
-                                ? bconfig(BCO_PARAMS::CHI)
-                                : bconfig.seq_setting(SEQ_SETTINGS::FINAL_CHI);
+  double const final_chi = bconfig.seq_setting(SEQ_SETTINGS::FINAL_CHI);
   double const initial_chi = bconfig(BCO_PARAMS::CHI);
 
   if (stage_enabled[STAGES::NOROT_BC]) {
