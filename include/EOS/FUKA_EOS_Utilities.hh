48e7109b88b497ac80c83b618900e6f4ad0f31c9
S.Tootle <sdtootle@gmail.com>
2025-11-28 17:11:15 -0700

    Add missing def

--- include/EOS/FUKA_EOS_Utilities.hh
+++ include/EOS/FUKA_EOS_Utilities.hh
@@ -49,6 +49,7 @@ struct set_eos_ope_struct {
         syst.add_ope("eps", &EOS<eos_t, EPSILON>::action, &p);
         syst.add_ope("press", &EOS<eos_t, PRESSURE>::action, &p);
         syst.add_ope("rho", &EOS<eos_t, DENSITY>::action, &p);
+        syst.add_ope("dHdlnrho", &EOS<eos_t, DHDRHO>::action, &p);
     }
 };
 
