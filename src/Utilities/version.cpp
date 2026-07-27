de4a48ec50b4fcfb7b1c37960aa20a76fbeebcbc
Samuel Tootle <sdtootle@gmail.com>
2026-07-27 20:40:08 +0200

    Add version interface

---
+++ src/Utilities/version.cpp
@@ -0,0 +1,19 @@
+// version.cpp
+
+#include "FUKA_Solvers/utilities/fuka_version.hpp"
+
+namespace Kadath::FUKA_Solvers {
+
+std::string git_hash() {
+    return GIT_HASH;
+}
+
+std::string git_describe() {
+    return GIT_DESCRIBE;
+}
+
+bool git_dirty() {
+    return GIT_DIRTY;
+}
+
+}  // namespace Kadath::FUKA_Solvers
