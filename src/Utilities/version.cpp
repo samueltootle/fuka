6bd9c3882bc8451cc1da76725460768ff107c0ae
Samuel Tootle <sdtootle@gmail.com>
2026-07-27 23:58:09 +0200

    add fuka version getter add tools to compare version tags

--- src/Utilities/version.cpp
+++ src/Utilities/version.cpp
@@ -4,6 +4,25 @@
 
 namespace Kadath::FUKA {
 
+std::tuple<int, int, int> parse_version(const std::string& v) {
+    int major = 0, minor = 0, patch = 0;
+    char dot;
+    std::istringstream iss(v);
+    iss >> major >> dot >> minor;
+    if (iss >> dot >> patch) {
+        // patch present
+    }
+    return {major, minor, patch};
+}
+
+bool is_older_than(const std::string& v, const std::string& target) {
+    return parse_version(v) < parse_version(target);
+}
+
+std::string fuka_version() {
+    return GIT_HASH;
+}
+
 std::string git_hash() {
     return GIT_HASH;
 }
